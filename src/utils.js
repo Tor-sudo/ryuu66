126e9dac8abbd9633434b205d734a5e06e45c7f4
Rushs321 <fyggh566@tutamail.com>
2024-08-20 16:38:52 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
 // utils.js
 
 function generateRandomIP() {
-  // Filter to ensure it doesn't fall within reserved IP ranges, adapt as needed
+  // Filter to ensure it doesn't fall within reserved IP ranges
   const randomOctet = () => Math.floor(Math.random() * 256);
 
-  // Simple check to avoid generating a private IP, adjust logic as needed
+  // Simple check to avoid generating a private IP
   let ip;
   do {
     ip = `${randomOctet()}.${randomOctet()}.${randomOctet()}.${randomOctet()}`;
@@ -24,7 +24,15 @@ function randomUserAgent() {
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
     'Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Mobile/15E148 Safari/604.1',
-    // Add more or dynamically fetch User-Agent strings from an external service if needed
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/40.0',
+    'Mozilla/5.0 (Linux; Android 9; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.93 Mobile Safari/537.36',
+    'Mozilla/5.0 (iPad; CPU OS 14_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Mobile/15E148 Safari/604.1',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+    'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5',
+    // Add more user agent strings as needed
   ];
 
   return userAgents[Math.floor(Math.random() * userAgents.length)];
