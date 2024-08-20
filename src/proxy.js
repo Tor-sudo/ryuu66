32f2e5b99b8e6a24446966745d87bd100c62691d
Rushs321 <fyggh566@tutamail.com>
2024-08-20 16:21:41 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,67 +1,66 @@
-const request = require('request');
-const { pick } = require('lodash'); // Import pick from lodash
-const { generateRandomIP, randomUserAgent } = require('./utils'); 
-const copyHeaders = require('./copyHeaders');
-const compress = require('./compress');
-const bypass = require('./bypass');
-const redirect = require('./redirect');
-const shouldCompress = require('./shouldCompress');
-
+const reqHandler = require('request');
+const { pick } = require('lodash');
+const { generateRandomIP, randomUserAgent } = require('./utils');
+const copyHdrs = require('./copyHeaders');
+const applyCompression = require('./compress');
+const performBypass = require('./bypass');
+const handleRedirect = require('./redirect');
+const checkCompression = require('./shouldCompress');
 
 // Array of predefined Via header values
-const viaOptions = [
-    '1.1 my-proxy-service.com (MyProxy/1.0)',
-    '1.0 my-other-proxy.net (AnotherProxy/2.0)',
-    '1.1 custom-proxy-system.org (CustomProxy/3.1)',
-    '1.1 some-other-proxy.com (DynamicProxy/4.0)',
+const viaHeaders = [
+    '1.1 example-proxy-service.com (ExampleProxy/1.0)',
+    '1.0 another-proxy.net (Proxy/2.0)',
+    '1.1 different-proxy-system.org (DifferentProxy/3.1)',
+    '1.1 some-proxy.com (GenericProxy/4.0)',
 ];
 
-function getRandomViaHeader() {
-    const randomIndex = Math.floor(Math.random() * viaOptions.length);
-    return viaOptions[randomIndex];
+function randomVia() {
+    const index = Math.floor(Math.random() * viaHeaders.length);
+    return viaHeaders[index];
 }
 
-function proxy(req, res) {
+function processRequest(req, res) {
     const { url, jpeg, bw, l } = req.query;
 
     // Handle the case where `url` is missing
     if (!url) {
-        const randomIP = generateRandomIP();
-        const userAgent = randomUserAgent();
-        const headers = {
+        const ipAddress = generateRandomIP();
+        const ua = randomUserAgent();
+        const hdrs = {
             ...pick(req.headers, ['cookie', 'dnt', 'referer']),
-            'x-forwarded-for': randomIP,
-            'user-agent': userAgent,
-            'via': getRandomViaHeader(), // Generate random Via header
+            'x-forwarded-for': ipAddress,
+            'user-agent': ua,
+            'via': randomVia(), // Generate random Via header
         };
 
         // Set headers and return an invalid request response
-        Object.keys(headers).forEach(key => res.setHeader(key, headers[key]));
+        Object.keys(hdrs).forEach(key => res.setHeader(key, hdrs[key]));
         
-    return res.end(`1we23`);
+        return res.end(`1we23`);
     }
 
     // Process and clean URL
-    const urls = Array.isArray(url) ? url.join('&url=') : url;
-    const cleanedUrl = urls.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
+    const urlList = Array.isArray(url) ? url.join('&url=') : url;
+    const cleanUrl = urlList.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
 
     // Setup request parameters
-    req.params.url = cleanedUrl;
+    req.params.url = cleanUrl;
     req.params.webp = !jpeg;
     req.params.grayscale = bw !== '0';
     req.params.quality = parseInt(l, 10) || 40;
 
-    const randomizedIP = generateRandomIP();
+    const randomIP = generateRandomIP();
     const userAgent = randomUserAgent();
 
     // Set up the request with the random Via header
-    request.get({
+    reqHandler.get({
         url: req.params.url,
         headers: {
             ...pick(req.headers, ['cookie', 'dnt', 'referer']),
             'user-agent': userAgent,
-            'x-forwarded-for': randomizedIP,
-            'via': getRandomViaHeader(), // Generate random Via header
+            'x-forwarded-for': randomIP,
+            'via': randomVia(), // Generate random Via header
         },
         timeout: 10000,
         maxRedirects: 5,
@@ -71,20 +70,20 @@ function proxy(req, res) {
         jar: true,
     }, (err, origin, buffer) => {
         if (err || origin.statusCode >= 400) {
-            return redirect(req, res);
+            return handleRedirect(req, res);
         }
 
-        copyHeaders(origin, res);
+        copyHdrs(origin, res);
         res.setHeader('content-encoding', 'identity');
         req.params.originType = origin.headers['content-type'] || '';
         req.params.originSize = buffer.length;
 
-        if (shouldCompress(req)) {
-            compress(req, res, buffer);
+        if (checkCompression(req)) {
+            applyCompression(req, res, buffer);
         } else {
-            bypass(req, res, buffer);
+            performBypass(req, res, buffer);
         }
     });
 }
 
-module.exports = proxy;
+module.exports = processRequest;
