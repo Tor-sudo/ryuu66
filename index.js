52812b8525ed97c14c0baf1d68290d3bd5c11e16
Rushs321 <fyggh566@tutamail.com>
2024-08-21 12:46:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 const app = require('express')();
 const proxy = require('./src/proxy');
 
-const PORT = process.env.PORT || 8080;
-
 app.enable('trust proxy');
 app.get('/', proxy);
 module.exports = app;
