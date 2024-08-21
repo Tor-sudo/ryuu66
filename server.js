7785f4107ea58d8e5c64be3a0c6fab4903f12313
Rushs321 <fyggh566@tutamail.com>
2024-08-21 09:16:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,5 +7,4 @@ const PORT = process.env.PORT || 8080;
 
 app.enable('trust proxy');
 app.get('/', proxy);
-app.get('/favicon.ico', (req, res) => res.status(204).end());
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
